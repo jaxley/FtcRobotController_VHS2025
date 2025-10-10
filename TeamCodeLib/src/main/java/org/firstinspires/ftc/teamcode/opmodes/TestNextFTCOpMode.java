51cd31ee8fbce181c23f81e5535642272a229eab
Jason Axley <jason@axley.net>
2025-10-10 07:10:58 -0700

    Split out non-Android code into TeamCodeLib to support loading OpModes in the virtual_robot simulator

---
+++ TeamCodeLib/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestNextFTCOpMode.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import dev.nextftc.ftc.NextFTCOpMode;
+
+@TeleOp
+public class TestNextFTCOpMode extends NextFTCOpMode {
+
+}
