216ea8b74ef4923abae9b87487c9327322bece81
Jason Axley <jason@axley.net>
2025-11-01 13:48:30 -0700

    Removing duplicate buildPaths call causing null pointer exception

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousOpMode.java
@@ -91,8 +91,6 @@ public class AutonomousOpMode extends OpMode {
 
     @Override
     public void init() {
-        buildPaths();
-
         pathTimer = new Timer();
         opmodeTimer = new Timer();
         opmodeTimer.resetTimer();
