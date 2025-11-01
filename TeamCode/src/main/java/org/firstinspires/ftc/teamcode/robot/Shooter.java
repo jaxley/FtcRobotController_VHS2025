6409d22c8e8d2aca9713b1b2d6b2009de776df6c
thecoolestwick <captainspencerpants@gmail.com>
2025-11-01 09:05:13 -0700

    Initial paths fpr autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.java
@@ -36,6 +36,8 @@ public class Shooter {
     //control vars
     boolean flywheelRunning = false;
 
+    boolean TELEOP_MODE = true; // default to TELEOP
+
     DButton fireButton = new DButton();
 
     public Shooter(DcMotorEx flywheel, Servo fireServo) {
@@ -79,6 +81,16 @@ public class Shooter {
             }
         }
        // fireTime -= getRuntime() - lastMillis;
+       // fireTime -= getRuntime() - lastMillis;
+    }
+
+    public Shooter withAutonomousMode(Telemetry telemetry) {
+        enableAutonomousMode(telemetry);
+        return this;
+    }
+
+    private void enableAutonomousMode(Telemetry telemetry) {
+        TELEOP_MODE = false;
     }
 
     public void stop(Telemetry telemetry) {
@@ -104,16 +116,5 @@ public class Shooter {
     public void reset(Telemetry telemetry) {
         telemetry.addData(SUBSYSTEM_NAME, "reset");
         fireServo.setPosition(fireDownPos);
-    }
-
-
-    private boolean TELEOP_MODE = true;
-    public Shooter withAutonomousMode(Telemetry telemetry) {
-        enableAutonomousMode(telemetry);
-        return this;
-    }
-
-    private void enableAutonomousMode(Telemetry telemetry) {
-        TELEOP_MODE = false;
     }
 }
