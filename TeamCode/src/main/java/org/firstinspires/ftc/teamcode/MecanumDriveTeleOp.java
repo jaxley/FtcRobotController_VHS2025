4ffac4c6a46a3ea5f2b40b5c435d123ace1fa71e
Jason Axley <jason@axley.net>
2025-11-04 16:12:54 -0800

    Added build time and version name to telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveTeleOp.java
@@ -1,20 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
-import static org.firstinspires.ftc.teamcode.robot.Constants.Motor;
-import static org.firstinspires.ftc.teamcode.robot.Constants.Wheel;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.robot.Intake;
 import org.firstinspires.ftc.teamcode.robot.RobotBase;
-import org.firstinspires.ftc.teamcode.robot.Shooter;
-import org.firstinspires.ftc.teamcode.robot.SimpleMecanumDrive;
 
 @TeleOp(name="TeleOp", group="Iterative Opmode")
 public class MecanumDriveTeleOp extends LinearOpMode {
@@ -23,7 +12,9 @@ public class MecanumDriveTeleOp extends LinearOpMode {
     public void runOpMode() {
         RobotBase robotBase = RobotBase.getInstance(hardwareMap);
 
-        telemetry.addData("Status", "initialized");
+        telemetry.addData("Code Version", BuildConfig.VERSION_NAME);
+        telemetry.addData("Code Build Time", BuildConfig.APP_BUILD_TIME);
+        telemetry.addData("TeleOp", "initialized");
         telemetry.update();
 
         waitForStart();
