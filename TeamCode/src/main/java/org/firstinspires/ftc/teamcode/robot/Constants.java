1ca21df359cea11884c1d2134166ea0a3420bd4f
Jason Axley <jason@axley.net>
2025-11-08 11:26:05 -0800

    Corrected hardware map constants for the encoder wheels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Constants.java
@@ -3,10 +3,10 @@ package org.firstinspires.ftc.teamcode.robot;
 public class Constants {
 
     public static final class Wheel {
-        public static final String FRONT_LEFT = "frontLeft";
-        public static final String FRONT_RIGHT = "frontRight";
-        public static final String BACK_LEFT = "backLeft";
-        public static final String BACK_RIGHT = "backRight";
+        public static final String FRONT_LEFT = "frontLeft"; // expansion hub port 0
+        public static final String FRONT_RIGHT = "frontRight"; // expansion hub port 1
+        public static final String BACK_LEFT = "backLeft"; // expansion hub port 2
+        public static final String BACK_RIGHT = "backRight"; // expansion hub port 3
     }
 
     public static final class Motor {
@@ -16,9 +16,9 @@ public class Constants {
     }
 
     public static final class EncoderWheel {
-        public static final String RIGHT = "enc_right";
-        public static final String LEFT = "enc_left";
-        public static final String CENTER = "enc_center";
+        public static final String RIGHT = Wheel.FRONT_LEFT; // "enc_right"; // port 0
+        public static final String LEFT = Wheel.BACK_LEFT; // "enc_left"; // port 2
+        public static final String CENTER = Wheel.FRONT_RIGHT; // "enc_center"; // port 1
     }
 
 }
