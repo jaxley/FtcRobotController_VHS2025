2f1ef7b6b633458b12da454c01bdbcc0ba74fb48
thecoolestwick <captainspencerpants@gmail.com>
2025-11-09 20:13:58 -0800

    Updated pedro pathing tuning parameters and poses

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Poses.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.pedropathing.geometry.Pose;
+
+public class Poses {
+    public static final Pose redShootingPose = new Pose(128, 80, Math.toRadians(90));
+    public static final Pose redLoadingPose = new Pose(130, 10, Math.toRadians(180));
+    public static final Pose blueleavePose = new Pose(125, 100, Math.toRadians(200));
+    //Pose blueShootingPose = new Pose(72, 84, Math.toRadians(135));
+    public static final Pose blueShootingPose = new Pose(16, 80, Math.toRadians(90));
+    public static final Pose blueLoadingPose = new Pose(15, 10, Math.toRadians(0));
+    public static final Pose blueLowLeavePose = new Pose(20, 8.5, Math.toRadians(330));
+    public static final Pose redIntakePoseR1 = new Pose(104, 37, Math.toRadians(0));
+    public static final Pose blueIntakePoseR1 = new Pose(40, 37, Math.toRadians(180));
+    public static final Pose redIntakePoseR2 = new Pose(104, 61, Math.toRadians(0));
+    public static final Pose blueIntakePoseR2 = new Pose(40, 61, Math.toRadians(180));
+    public static final Pose redIntakePoseR3 = new Pose(104, 85, Math.toRadians(0));
+    public static final Pose blueIntakePoseR3 = new Pose(40, 85, Math.toRadians(180));
+    public static final Pose blueStartingPose1 = new Pose(60, 8.5, Math.toRadians(90));
+    public static final Pose redStartingPose2 = new Pose(108, 134, Math.toRadians(37));
+    public static final Pose redStartingPose1 = new Pose(83, 8.5, Math.toRadians(90));
+    public static final Pose blueStartingPose2 = new Pose(24, 128, Math.toRadians(37));
+    public static final Pose redLowLeavePose = new Pose(108 ,8.5, Math.toRadians(90));
+    public static final Pose redTopLeavePose = new Pose(98, 120, Math.toRadians(0));
+}
