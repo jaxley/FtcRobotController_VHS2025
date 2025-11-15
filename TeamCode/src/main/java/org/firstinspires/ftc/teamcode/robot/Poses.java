3cbd72001f107800066c176e41d1c0ae41469aa0
thecoolestwick <captainspencerpants@gmail.com>
2025-11-15 12:41:39 -0800

    Fixed a broken heading.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Poses.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Poses.java
@@ -58,12 +58,12 @@ public class Poses {
 
     // Verify or create these using https://visualizer.pedropathing.com
     public static final Pose redShootingPose = new Pose(128, 80, Math.toRadians(90)); // TODO - incorrect - this is on top of the first row of balls
-    public static final Pose redLoadingPose = new Pose(130, 10, Math.toRadians(180)); // TODO - correct location WRONG alliance. This is on the blue alliance side (goals are opposite the alliance areas)
+    public static final Pose redLoadingPose = new Pose(130, 10, Math.toRadians(180)); //TODO - correct location WRONG alliance. This is on the blue alliance side (goals are opposite the alliance areas)
     public static final Pose blueleavePose = new Pose(125, 100, Math.toRadians(200)); // TODO - correct location WRONG alliance. This is near the red goal.
     //Pose blueShootingPose = new Pose(72, 84, Math.toRadians(135));
     public static final Pose blueShootingPose = new Pose(16, 80, Math.toRadians(90)); // TODO - incorrect. same issue as redShootingPose but opposite side
     public static final Pose blueLoadingPose = new Pose(15, 10, Math.toRadians(0)); // TODO - correct location WRONG alliance.
-    public static final Pose blueLowLeavePose = new Pose(36, 8.5, Math.toRadians(330));
+    public static final Pose blueLowLeavePose = new Pose(36, 8.5, Math.toRadians(90));
     public static final Pose redIntakePoseR1 = new Pose(104, 37, Math.toRadians(0)); // correct. R1 is bottom of the field
     public static final Pose blueIntakePoseR1 = new Pose(40, 37, Math.toRadians(180)); // correct.
     public static final Pose redIntakePoseR2 = new Pose(104, 61, Math.toRadians(0)); // correct.
@@ -75,7 +75,7 @@ public class Poses {
     public static final Pose redStartingPoseLow_D1 = new Pose(83, 8.5, Math.toRadians(90)); // correct
     public static final Pose blueStartingPoseTop_C6 = new Pose(63, 135, Math.toRadians(180)); // correct.
     public static final Pose blueStartingPoseTop_B6 = new Pose(33, 135, Math.toRadians(180)); // correct.
-    public static final Pose blueStartingPoseLow_ = new Pose(60, 8.5, Math.toRadians(90)); // correct.
+    public static final Pose blueStartingPoseLow_C1 = new Pose(60, 8.5, Math.toRadians(90)); // correct.
 
     public static final Pose redLowLeavePose = new Pose(108 ,8.5, Math.toRadians(90)); // TODO - correct location. WRONG alliance. Swap with blueLowLeavePose
     public static final Pose redTopLeavePose = new Pose(98, 120, Math.toRadians(0)); // TODO - This is wrong. Still inside the launch zone
