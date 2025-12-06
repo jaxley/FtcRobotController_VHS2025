ee9ff7c5f142cf7b9735bdc57bf66d0f8c48baf6
thecoolestwick <captainspencerpants@gmail.com>
2025-12-06 13:10:54 -0800

    Updated the home position of the firing servo.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Shooter.java
@@ -27,7 +27,7 @@ public class Shooter {
     double flywheelPower = -0.8;
     final double FLYWHEEL_RPM_2_CLICKS_PER_SECOND_CONVERSION = (double) 28 /60;
 
-    final double fireDownPos = 0;
+    final double fireDownPos = 0.2;
     final double fireUpPos= 0.5;
     final double firePeriodMs = 800; // ms
 
